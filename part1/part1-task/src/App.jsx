f190fb29ab0711ef2e39bf54a5f4979b55b2fffb
Rafsan Habib <rafsanhabib97@gmail.com>
2023-11-26 12:25:28 +0600

    Component state, event handlers

--- part1/part1-task/src/App.jsx
+++ part1/part1-task/src/App.jsx
@@ -1,52 +1,107 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
-function Hello (props){
-  const now = new Date()
+const Hello = ({name, age}) => {
   //const a = props.a
   //const b = props.b
-  console.log(props)
+  // const {name, age} = props
+  // console.log(props)
+
+  // function bornYear (){
+  //   const yearNow= new Date().getFullYear()
+  //   return yearNow - age
+  // }
+
+  const bornYear = () => Number(new Date().getFullYear()) - age
   return (
     <>
-      <div>
+      {/* <div>
         <p>Hello World, it is {now.toDateString()}</p>
         <p>{props.a} plus {props.b} is {props.a+props.b}</p>
         <p>{props.a+1} plus {props.b+1} is {props.a+props.b+2}</p>
-      </div>
+      </div> */}
+      <p>
+        Hello {name}, you are {age} years old.
+      </p>
+      <p>You were probably born in {bornYear()}</p>
     </>
   )
 }
-function App() {
-  const [count, setCount] = useState(0)
-  //const a = 99
-  //const b = 69
+
+// Destructuring of props example
+const Display = ({counter}) => {
+  return (
+    <div>
+      {counter}
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+
+
+
+function App(props) {
+  // const {counter} = props
+  // const name = 'Peter'
+  // const age = 69
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+  // setTimeout( 
+  //   () => setCounter(counter + 1), 
+  //   1000
+  // )
+
+  //console.log("rendering..", counter)
+  
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const reset = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {/* line 48 uses variables/js expressions -> custom Edit
-       line 49 uses hardcoded values -> custom Edit */}
-      {/* <Hello a={a} b={b}/> 
+      {/* next third line uses variables/js expressions -> custom Edit
+       next fourth line uses hardcoded values -> custom Edit */}
+      {/* <Hello a={a} b={b}/>
       <Hello a={2} b={4}/> */}
+      {/* <h1>Greetings</h1>
+      <Hello name="Maya" age={42 + 10}/>
+      <Hello name={name} age={age} /> */}
+      <Display counter = {counter}/>
+      <Button 
+      onClick={increaseByOne}
+      text='Click me to increase!'
+      />
+      <Button
+      onClick={reset}
+      text='Click me to reset'
+      />
+      <Button
+      onClick={decreaseByOne}
+      text='Click me to decrease'
+      />
 
     </>
   )
--- part1/part1-task/src/main.jsx
+++ part1/part1-task/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+let counter = 0
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <App />
+    <App counter={counter}/>
   // </React.StrictMode>,
 )
