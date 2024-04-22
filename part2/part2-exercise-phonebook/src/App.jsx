a719ce15aacf32dd91c4a2c06be2548067828674
Rafsan Habib <rafsanhabib97@gmail.com>
2024-04-22 12:04:04 +0600

    improved notification functionalities

--- part2/part2-exercise-phonebook/src/App.jsx
+++ part2/part2-exercise-phonebook/src/App.jsx
@@ -2,12 +2,28 @@ import { useEffect, useState } from 'react'
 import pbService from './services/phonebook'
 
 const empStr = "";
+const SuccessNotification = ({message}) => {
+  if(message === null) return null
+  return (
+    <div className='success'>
+      {message}
+    </div>
+  )
+}
+const ErrorNotification = ({message}) => {
+  if(message === null) return null
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 
 const ContactList = (props) => {
   const contacts = props.persons
   const filterString = props.filterString
   const deleteContact = props.deleteContact
-  const deleteId = 'del'
 
   if(filterString === ''){
     return (
@@ -66,6 +82,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+  const [success, setSuccess] = useState(null)
+  const [error, setError] = useState(null)
 
   // fetch initial phonebook data from server (initDB.json)
   useEffect( 
@@ -105,7 +123,7 @@ const App = () => {
       // window.alert(`${newName} is already added to phonebook`)
       return condObj
     }
-    // setPersons(persons.concat(personObj))
+  
     return newName
   }
   
@@ -126,6 +144,7 @@ const App = () => {
     const addingNumber = validateNumOp(event)
     const addingId = String(persons.length + 1);
 
+    // Conditional Validation //
     const nameObj = typeof addingName === 'object' &&
     !Array.isArray(addingName) &&
     addingName !== null
@@ -136,6 +155,7 @@ const App = () => {
 
     const newName = typeof addingName === 'string'
     const newNum = typeof addingNumber === 'string'
+    // Conditional Validation //
 
     // a contact with same name exists already
     if(nameObj && !numObj) {
@@ -153,20 +173,32 @@ const App = () => {
                 })
               )
             })
+            .catch(error => {
+              setError(`Information of ${addingName.name} has already been removed from server`)
+              setTimeout(() => {setError(null)}, 3000)
+              setPersons(persons.filter(person => person.id !== addingName.id))
+            })
         }
-    } 
+    }
+
+    //a contact with same number exists already
+    if(!nameObj && numObj)  {
+      alert("Same number can't be used for two different persons.")
+    }
+
+    // a fully new Contact
     if(newName && newNum) {
       const newPersonObj = {name:addingName, number:addingNumber, id:addingId}
       pbService
         .create(newPersonObj)
         .then(returnedState => {
           setPersons(persons.concat(newPersonObj))
+          setSuccess(`added ${newPersonObj.name}`)
+          setTimeout(() => setSuccess(null), 3000)
         })
     }
-    // else {
-    //   window.alert(`${addingName} or ${addingNumber} already exists or you're providing invalid value`)
-    // }
-    
+
+    // reset the input fields
     document.getElementById('nameField').value = ''
     document.getElementById('numField').value = '' 
   }
@@ -189,6 +221,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <SuccessNotification message={success} />
+      <ErrorNotification message={error} />
       <Filter searchNameOp={searchNameOp} />
       <h2>Add a new</h2>
       <PersonForm add2Phone={add2Phone} handleNameAddOp={handleNameAddOp} handleNumAddOp={handleNumAddOp}/>
